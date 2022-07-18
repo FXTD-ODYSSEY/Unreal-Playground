9cfa46b9e6a43d9af41f4c5fd077bf59b4c71997
timmyliang <timmyliang@tencent.com>
2022-07-18 10:33:32 +0800

    feat: add compile python script

--- compile.py
+++ compile.py
@@ -16,6 +16,11 @@ import json
 import os
 import glob
 import subprocess
+import logging
+import argparse
+
+logging.basicConfig(level=logging.INFO)
+
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -25,12 +30,11 @@ data_path = os.path.join(
 )
 
 
-def main():
+def get_ue_location():
     with open(data_path, "r") as rf:
         ue_data = json.load(rf)
 
     for uproject in glob.iglob(os.path.join(DIR, "*.uproject")):
-        # project_name = os.path.basename(os.path.splitext(uproject)[0])
         with open(uproject, "r") as rf:
             project_data = json.load(rf)
 
@@ -41,17 +45,80 @@ def main():
             app_name = app.get("ArtifactId")
             version = app_name.split("_")[-1]
             if version == project_version:
-                break
-        
-        print(ue_location)
-        UBT = os.path.join(ue_location, "Engine", "Binaries", "DotNET", "UnrealBuildTool.exe")
-        command = " ".join([UBT,'Development','Win64','-Project="{proj}"'.format(proj=uproject),'-TargetType=Editor' , "-Progress", "-NoEngineChanges"])
-        print(command)
-        subprocess.run(command)
-        
-    
+                return uproject, ue_location
+
+
+def run_command(command):
+    logging.info(command)
+    subprocess.run(command)
+
+
+def main():
+    collections = get_ue_location()
+    if not collections:
+        logging.error("No UE project or engine location found")
+        return
+    uproject, ue_location = collections
+
+    UBT = os.path.join(
+        ue_location, "Engine", "Binaries", "DotNET", "UnrealBuildTool.exe"
+    )
+    UE = os.path.join(ue_location, "Engine", "Binaries", "Win64", "UE4Editor.exe")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-a",
+        "--action",
+        help="display a square of a given number",
+        type=str,
+        default="",
+    )
+
+    args = parser.parse_args()
+    action = args.action.lower()
+
+    if action in ("generate_project", "gp"):
+        run_command(
+            " ".join(
+                [
+                    UBT,
+                    "-projectfiles",
+                    '-Project="{proj}"'.format(proj=uproject),
+                    "-game",
+                    "-engine",
+                    "-progress",
+                ]
+            )
+        )
+    if action in ("launch_project", "lp"):
+        run_command(
+            " ".join(
+                [
+                    UE,
+                    uproject,
+                    "-skipcompile",
+                ]
+            )
+        )
+    else:
+        # NOTES(timmyliang): compile project
+        run_command(
+            " ".join(
+                [
+                    UBT,
+                    "Development",
+                    "Win64",
+                    '-Project="{proj}"'.format(proj=uproject),
+                    "-TargetType=Editor",
+                    "-Progress",
+                    "-NoEngineChanges",
+                ]
+            )
+        )
+
+
 # D:/EpicGames/UE_4.27/Engine/Binaries/DotNET/UnrealBuildTool.exe Development Win64 -Project="D:/EpicGames/Unreal_Playground/Unreal_Playground.uproject" -TargetType=Editor -Progress -NoEngineChanges -NoHotReloadFromIDE
-# Engine/Binaries/DotNET/UnrealBuildTool.exe  -projectfiles -project="D:/EpicGames/test_plugin/test_plugin.uproject" -game -engine -progress 
+# Engine/Binaries/DotNET/UnrealBuildTool.exe  -projectfiles -project="D:/EpicGames/test_plugin/test_plugin.uproject" -game -engine -progress
 
 if __name__ == "__main__":
     main()
